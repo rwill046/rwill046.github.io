9aa6bd5e9ed6dd70732088b5922ff65fc7e7ebf9
Rohaan Williams <rwill046@uottawa.ca>
2020-07-19 23:15:54 -0400

    Lab08

---
+++ Lab08/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Gallery from './gallery/Gallery';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Gallery />
+        <p>
+          With Delicious Desserts have your taste personified. 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
